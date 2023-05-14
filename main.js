1b5151871cf216d8af01610f2471358651d757cc
Israel de Souza <apps.sites.sistematico@gmail.com>
2023-05-14 20:19:38 -0300

    logica v1 finalizada, amanha fazer breves ajustes

--- main.js
+++ main.js
@@ -2,39 +2,15 @@
 // Fazer validação para SO RECEBER ENTRE 1 E 10
 
 // VARIAVEIS
-var cadeira = document.getElementById('numeroCadeira');
 var exibirCadeiras = document.getElementById('exibirCadeiras');
 const enviar = document.getElementById('enviar');
 
 var resposta = '';
-var reserva = [];
 const QNTD_CADEIRAS = 10;
+var reserva = [];
+var cadeira = document.getElementById('numeroCadeira');
 
-limparEExibirCadeirasCinema()
-
-enviar.addEventListener('click', () => {
-    
-    inserirValoresEmReservaEVerificar()
-    
-    
-    do {
-        limparEExibirCadeirasCinema()
-        
-        
-        
-        
-        
-        
-        resposta = prompt('Deseja continuar? [s/n]');
-        
-    } while (resposta == 's');
-
-    if (resposta != 's') {
-        alert('Todos os assentos escolhidos foram reservados!')
-    }
-
-})
-
+// FUNÇÕES
 function limparEExibirCadeirasCinema() {
     exibirCadeiras.innerHTML = '';
     for (let count = 1; count <= QNTD_CADEIRAS; count++) {
@@ -44,49 +20,43 @@ function limparEExibirCadeirasCinema() {
     }
 }
 
-
-function inserirValoresEmReservaEVerificar() {
-    for(c = 1; c <= QNTD_CADEIRAS; c++) {
-        reserva[c] = c;
-    }
-
-    cadeiraSelecionada = Number(cadeira.value);
-    
+function inserirValoresEmReservaEVerificar(valorDigitado) {
     for (c = 1; c <= QNTD_CADEIRAS; c++) {
-
-        if( Number(cadeira.value) == reserva[c]){
-            reserva[c] = '---';
-
-            if (reserva[c] == '---') {
-                alert('Erro, lugar já reservado');
-            }
-
+        if( reserva[valorDigitado] ==  ' RESERVADO '){
+            return false
+        } else {
+            return true
         }
-
-        console.log(reserva[c])
     }
 }
 
+for (c = 1; c <= QNTD_CADEIRAS; c++) {
+    reserva[c] = c
+};
 
+for (let count = 1; count <= QNTD_CADEIRAS; count++) {
+    span = document.createElement('span');             
+    span.innerHTML = ` | ${count} | `
+    exibirCadeiras.appendChild(span);
+}
 
-/* 
-enviarNumero.addEventListener('click', () => {
+enviar.addEventListener('click', () => {
+        
+    cadeiraSelecionada = Number(cadeira.value);
+
+    inserirValoresEmReservaEVerificar(cadeiraSelecionada)
     
-    do {
-    
-        for(index = 1; index <= QNTD_CADEIRAS ;index++) {
-            if(cadeira == reserva[index] ){
-                reserva[index] = '---';
+    if (inserirValoresEmReservaEVerificar(cadeiraSelecionada) == false) {
+        alert('Cadeira Reservada, tente novamente!');
+    } else {
+        
+        for (let contador = 0; contador <= QNTD_CADEIRAS  ; contador++) {
+            if (reserva[contador] == cadeiraSelecionada) {
+                reserva[contador] = ' RESERVADO ';
             }
         }
-    
-        resposta = prompt('Quer continuar? [s/n] ')
-        
-    } while (resposta == 's');
 
-    if (resposta != 's') {
-        console.log('programa encerrado');
+        exibirCadeiras.innerHTML = '';
+        exibirCadeiras.innerHTML += reserva.join(' | ');
     }
-
-});
- */
+})
